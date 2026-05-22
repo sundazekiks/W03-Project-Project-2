a5d53de135b282324cd163ffb8241e61933858dd
sundazekiks <abutontravis1@gmail.com>
2026-05-22 15:53:29 -0400

    fixed: moved the swagger autogen lib to deps

--- swagger.js
+++ swagger.js
@@ -1,12 +1,16 @@
 const swaggerAutogen = require('swagger-autogen');
+const { config } = require('dotenv');
 
+config();
+
+const host = process.env.NODE_ENV === 'production' ? process.env.PRODUCTION_HOST : 'localhost:8081';
 
 const doc = {
     info: {
         title: 'CSE341 API',
         description: 'API for CSE341 project'
     },
-    host: 'localhost:8081',
+    host: host,
     schemes: ['http', 'https'],
 };
 
